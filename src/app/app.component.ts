aa8799aa24c48c23d26326f2a65123cc9181babb
Zqang <alvinang1406@gmail.com>
2024-02-26 22:37:48 +0800

    done

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,12 @@
+import { CommonModule, NgOptimizedImage } from '@angular/common';
 import { Component } from '@angular/core';
-import { CommonModule } from '@angular/common';
-import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet],
+  imports: [CommonModule, NgOptimizedImage],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  styleUrl: './app.component.scss',
 })
 export class AppComponent {
   title = 'qr-code-component';
